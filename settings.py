b3ced871f8a7043c40618c3e0147a9dd0d0b7e73
Hüsnü Əhədov <80684361+sirincay@users.noreply.github.com>
2022-03-24 13:20:21 +0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
-TOKEN = "5278187676:AAHqcu-pjYu7Mb4TFjE_Sx6s76aQjxljUOk"
+TOKEN = "TOKEN"
 with open('hüsnü.reqemlist.txt', 'r', encoding='utf-8') as file:
     word_list = file.read().splitlines()
